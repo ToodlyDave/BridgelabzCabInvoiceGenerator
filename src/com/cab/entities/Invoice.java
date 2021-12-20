a5f3459a6c2e5f85a6de55bbe9b0974378c250ea
ToodlyDave <alapatdavid@gmail.com>
2021-12-20 17:22:26 +0530

    ADD method to handle multiple categories of rides

--- src/com/cab/entities/Invoice.java
+++ src/com/cab/entities/Invoice.java
@@ -26,7 +26,10 @@ public class Invoice {
 				&& rideCount == other.rideCount
 				&& Double.doubleToLongBits(totalFare) == Double.doubleToLongBits(other.totalFare);
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "Invoice [rideCount=" + rideCount + ", totalFare=" + totalFare + ", avgFare=" + avgFare + "]";
+	}
 
 }
